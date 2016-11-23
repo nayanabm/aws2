358f97881fb75d0c9a6e91d384d21f6a7ddfdfcf
nayanabm <bnayana@thoughtworks.com>
2016-11-23 22:45:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ sudo usermod -a -G docker ec2-user #grant root user for docker command
 sudo pip install docker-compose #installs docker-compose
 #installation of R on virtual machine
 sudo rpm -Uvh http://www.nic.funet.fi/pub/mirrors/fedora.redhat.com/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
+sudo yum install -y R
 #postgres setup
 sudo yum install -y postgresql95 postgresql95-server postgresql95-libs postgresql95-contrib postgresql95-devel
