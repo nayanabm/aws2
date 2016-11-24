faf50d36d05b06eea78e1dd4ecfc9a47c463e015
nayanabm <bnayana@thoughtworks.com>
2016-11-24 16:40:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo yum install -y docker #docker installation
 sudo service docker start #starts docker service : reboot the virtual machine when it gets done
 sudo usermod -a -G docker ec2-user #grant root user for docker command
+sudo easy_install pip
 sudo pip install docker-compose #installs docker-compose
 #installation of R on virtual machine
 sudo rpm -Uvh http://www.nic.funet.fi/pub/mirrors/fedora.redhat.com/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
